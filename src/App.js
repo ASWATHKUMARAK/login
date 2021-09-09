1d9d37d0d1ac05ae5a6e0943fc00d9a4b6bdd414
ASWATHKUMARAK <89625623+ASWATHKUMARAK@users.noreply.github.com>
2021-09-09 13:19:22 +0530

    create a login page for Cgate

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">  
+      <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+         </header>
+    </div>
+  );
+}
+
+export default App;
